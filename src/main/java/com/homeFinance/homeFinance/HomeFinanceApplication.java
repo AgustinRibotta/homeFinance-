c2061ba30572bd0bc05e082a35aa105816734e64
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2026-06-21 13:01:47 +0200

    first commit

---
+++ src/main/java/com/homeFinance/homeFinance/HomeFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.homeFinance.homeFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeFinanceApplication.class, args);
+	}
+
+}
