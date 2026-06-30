f1cc7a62ace01fe4390eff332ed8fc74ff330846
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2026-06-30 22:20:48 +0200

    Feat: Period getter, setter, constructor generate. Feat: Balance entity.

--- src/main/java/com/homeFinance/homeFinance/entity/Household.java
+++ src/main/java/com/homeFinance/homeFinance/entity/Household.java
@@ -1,17 +1,9 @@
 package com.homeFinance.homeFinance.entity;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 import java.util.UUID;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "household")
@@ -27,37 +19,37 @@ public class Household {
     @OneToMany(mappedBy = "household", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<User> user;
 
-	public Household() {
-	}
+    public Household() {
+    }
 
-	public Household(UUID id, String name, List<User> user) {
-		this.id = id;
-		this.name = name;
-		this.user = user;
-	}
+    public Household(UUID id, String name, List<User> user) {
+        this.id = id;
+        this.name = name;
+        this.user = user;
+    }
 
-	public UUID getId() {
-		return id;
-	}
+    public UUID getId() {
+        return id;
+    }
 
-	public void setId(UUID id) {
-		this.id = id;
-	}
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public List<User> getUser() {
-		return user;
-	}
+    public List<User> getUser() {
+        return user;
+    }
 
-	public void setUser(List<User> user) {
-		this.user = user;
-	}
+    public void setUser(List<User> user) {
+        this.user = user;
+    }
 
 }
